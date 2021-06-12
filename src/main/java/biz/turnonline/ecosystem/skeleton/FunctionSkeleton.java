66f2e147a32c846e2c165303120aab3be4683e58
Aurel Medvegy <medvegy@turnonline.biz>
2021-06-12 07:18:54 +0200

    Firestore event extended by additional useful properties as updateMask (fieldPaths), fields, and updateTime.

--- src/main/java/biz/turnonline/ecosystem/skeleton/FunctionSkeleton.java
+++ src/main/java/biz/turnonline/ecosystem/skeleton/FunctionSkeleton.java
@@ -10,6 +10,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 import java.util.StringJoiner;
 
 /**
@@ -59,12 +61,15 @@ public class FunctionSkeleton
 
         FirestoreValue oldValue;
 
+        UpdateMask updateMask;
+
         @Override
         public String toString()
         {
             return new StringJoiner( ", ", FirestoreEvent.class.getSimpleName() + "[", "]" )
                     .add( "value=" + value )
                     .add( "oldValue=" + oldValue )
+                    .add( "updateMask=" + updateMask )
                     .toString();
         }
     }
@@ -73,14 +78,33 @@ public class FunctionSkeleton
     {
         Date createTime;
 
+        Map<String, Object> fields;
+
         String name;
+
+        Date updateTime;
 
         @Override
         public String toString()
         {
             return new StringJoiner( ", ", FirestoreValue.class.getSimpleName() + "[", "]" )
                     .add( "createTime='" + createTime + "'" )
+                    .add( "fields='" + fields + "'" )
                     .add( "name='" + name + "'" )
+                    .add( "updateTime='" + updateTime + "'" )
+                    .toString();
+        }
+    }
+
+    public static class UpdateMask
+    {
+        List<String> fieldPaths;
+
+        @Override
+        public String toString()
+        {
+            return new StringJoiner( ", ", UpdateMask.class.getSimpleName() + "[", "]" )
+                    .add( "fieldPaths=" + fieldPaths )
                     .toString();
         }
     }
