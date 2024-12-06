a5f1a2bbd838eaecad8b65ac4c35dec178354f64
Aurel Medvegy <medvegy@turnonline.biz>
2024-12-06 17:39:24 +0100

    Refactored to use CloudEventsFunction. In the unit test, the DocumentEventData reads from firestore-event.json, which will be supplied to the CloudEvent as the application/protobuf type.

--- src/main/java/biz/turnonline/ecosystem/skeleton/FunctionSkeleton.java
+++ src/main/java/biz/turnonline/ecosystem/skeleton/FunctionSkeleton.java
@@ -2,14 +2,16 @@ package biz.turnonline.ecosystem.skeleton;
 
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.FirestoreOptions;
-import com.google.cloud.functions.BackgroundFunction;
-import com.google.cloud.functions.Context;
+import com.google.cloud.functions.CloudEventsFunction;
 import com.google.common.annotations.VisibleForTesting;
+import com.google.events.cloud.firestore.v1.DocumentEventData;
 import com.google.gson.Gson;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.ProtocolStringList;
+import com.google.protobuf.util.JsonFormat;
+import io.cloudevents.CloudEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Arrays;
 
 /**
  * Entry point that listens for Firestore events.
@@ -17,7 +19,7 @@ import java.util.Arrays;
  * @author <a href="mailto:medvegy@turnonline.biz">Aurel Medvegy</a>
  */
 public class FunctionSkeleton
-        implements BackgroundFunction<FirestoreEvent>
+        implements CloudEventsFunction
 {
     private static final Logger LOGGER = LoggerFactory.getLogger( FunctionSkeleton.class );
 
@@ -39,17 +41,29 @@ public class FunctionSkeleton
     }
 
     @Override
-    public void accept( FirestoreEvent event, Context context )
+    public void accept( CloudEvent event ) throws InvalidProtocolBufferException
     {
-        LOGGER.info( context.attributes().toString() );
-        LOGGER.info( context.eventId() );
-        LOGGER.info( context.eventType() );
-        LOGGER.info( context.timestamp() );
-        LOGGER.info( "Function triggered by change to: " + event );
+        if ( event.getData() == null )
+        {
+            LOGGER.warn( "Event's data is not provided [{}]", event );
+            return;
+        }
 
-        final String fullDocPath = context.resource();
-        String[] strings = fullDocPath.split( "/" );
-        LOGGER.info( "Doc path fragments " + Arrays.toString( strings ) );
+        DocumentEventData data = DocumentEventData.parseFrom( event.getData().toBytes() );
+        ProtocolStringList whatHasChanged = data.getUpdateMask().getFieldPathsList();
+        LOGGER.info( "What has changed: {}", whatHasChanged );
+
+        // Parse the Protobuf data into a DocumentEventData object
+        DocumentEventData.Builder builder = DocumentEventData.newBuilder();
+        builder.mergeFrom( event.getData().toBytes() );
+        // Convert Protobuf object to JSON,
+        String json = JsonFormat.printer()
+                // This ensures that no extra whitespace is added, resulting in an effective copy in the log
+                .omittingInsignificantWhitespace()
+                .alwaysPrintFieldsWithNoPresence()
+                .print( builder );
+
+        LOGGER.info( json );
     }
 
 }
--- src/test/java/biz/turnonline/ecosystem/skeleton/FunctionSkeletonTest.java
+++ src/test/java/biz/turnonline/ecosystem/skeleton/FunctionSkeletonTest.java
@@ -1,9 +1,11 @@
 package biz.turnonline.ecosystem.skeleton;
 
 import com.google.cloud.firestore.Firestore;
-import com.google.cloud.functions.Context;
-import com.google.gson.Gson;
-import org.junit.jupiter.api.BeforeEach;
+import com.google.events.cloud.firestore.v1.DocumentEventData;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
+import io.cloudevents.CloudEvent;
+import io.cloudevents.core.builder.CloudEventBuilder;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Answers;
@@ -14,10 +16,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.net.URI;
 import java.util.stream.Collectors;
-
-import static org.mockito.Mockito.when;
 
 /**
  * {@link FunctionSkeleton} unit testing.
@@ -27,32 +27,11 @@ import static org.mockito.Mockito.when;
 @ExtendWith( MockitoExtension.class )
 public class FunctionSkeletonTest
 {
-    private static final String GOOGLE_CLOUD_PROJECT;
-
-    static
-    {
-        // Google Project ID setting to the value taken from firestore-event.json
-        FirestoreEvent event = readEvent();
-        GOOGLE_CLOUD_PROJECT = event.value.name.split( "/" )[1];
-        System.setProperty( "GOOGLE_CLOUD_PROJECT", GOOGLE_CLOUD_PROJECT );
-    }
-
     @InjectMocks
     private FunctionSkeleton tested;
 
-    private final String projectId = GOOGLE_CLOUD_PROJECT;
-
     @Mock( answer = Answers.RETURNS_DEEP_STUBS )
     private Firestore db;
-
-    @Mock
-    private Context context;
-
-    private static FirestoreEvent readEvent()
-    {
-        String json = readString( "firestore-event.json" );
-        return new Gson().fromJson( json, FirestoreEvent.class );
-    }
 
     /**
      * Reads the content of the file in the same package as this test and converts it into a string.
@@ -72,22 +51,19 @@ public class FunctionSkeletonTest
                 .collect( Collectors.joining( System.lineSeparator() ) );
     }
 
-    @BeforeEach
-    public void before()
-    {
-        when( context.attributes() ).thenReturn( new HashMap<>() );
-        when( context.eventId() ).thenReturn( "5c196d9d-8e36-8414-3q98-3dfd871e9b42-9" );
-        when( context.eventType() ).thenReturn( "providers/cloud.firestore/eventTypes/document.write" );
-        when( context.resource() ).thenReturn( "projects/test-1abc/databases/(default)/documents/accounts/Uyg...Nfvg/orders/VTb...SN" );
-        when( context.timestamp() ).thenReturn( "2020-12-18T12:44:23.883567Z" );
-    }
-
     @Test
-    public void validInputBody()
+    public void validInputBody() throws InvalidProtocolBufferException
     {
         String json = readString( "firestore-event.json" );
-        FirestoreEvent message = new Gson().fromJson( json, FirestoreEvent.class );
+        DocumentEventData.Builder builder = DocumentEventData.newBuilder();
+        JsonFormat.parser().merge( json, builder );
 
-        tested.accept( message, context );
+        CloudEvent event = CloudEventBuilder.v1()
+                .withId( "1" )
+                .withType( "google.cloud.firestore.document.v1.updated" )
+                .withSource( URI.create( "https://cloudevents.io" ) )
+                .withData( "application/protobuf", builder.build().toByteArray() )
+                .build();
+        tested.accept( event );
     }
 }
